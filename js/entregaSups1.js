8d31b2aa2be174991fa378ba5e0f5626f65bca91
Oduad <aragon_1123@hotmail.com>
2022-06-24 20:34:41 -0500

    Modificaciones de export/import

--- js/entregaSups1.js
+++ js/entregaSups1.js
@@ -6,7 +6,7 @@ const mostrarProductos = (productosLlegados) => {
     productosLlegados.forEach(producto => {
         const div = document.createElement('div');
         div.classList.add('card');  //class lee las clases en productos- contenedor y add es Para agregar una clase(card) en ese div
-        div.innerHTML += `<div class="card" style="width: 18rem;">
+        div.innerHTML += `<div class="card" style="width: 8rem;">
                     <img src="${producto.img}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${producto.nombre}</h5>
@@ -23,3 +23,24 @@ const mostrarProductos = (productosLlegados) => {
 mostrarProductos(productosDisponibles)
 
 //Duda 1: ¿En las líneas 3 y ___ no sería mejor var?
+
+/*const productosDisponibles = [
+    {
+        id: 1,
+        nombre: "Gold Whwy",
+        precio: 1200,
+        img: '../img/goldStWhey.jpg'
+    },
+    {
+        id: 2,
+        nombre: "Isopure",
+        precio: 1400,
+        img: '../img/isopure.jpg'
+    },
+    {
+        id: 3,
+        nombre: "Hexapro",
+        precio: 1100,
+        img: '../img/hexapro.jpg'
+    }
+]*/

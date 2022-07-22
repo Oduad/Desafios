d369521f888d8246b2c744faebd60bcd49fc43d2
Oduad <aragon_1123@hotmail.com>
2022-07-22 02:34:06 -0500

    Desarrollo de calculadora

--- jsCalcu/calculadora.js
+++ jsCalcu/calculadora.js
@@ -1,9 +1,39 @@
+const swa = (mensaje, bgColor, tiempo, icono, color) => {
+    Swal.fire({
+        icon: icono || '',
+        title: mensaje,
+        position: 'top-center',
+        showConfirmButton: true,
+        toast: true,
+        timer: tiempo || 3000,
+        timerProgressBar: true,
+        background: bgColor || 'white',
+        color: color || '#ffffff'
+    })
+}//Falta arreglar el Swal 
+
 const boton = document.getElementById('calcular');
 
 boton.addEventListener('click', () => {
+    let nombre = document.getElementById('inputName').value;
     let masa = Number(document.getElementById("inputWeight").value);
     let altura = Number(document.getElementById('inputHeight').value);
-    let imc = masa / (altura * altura)
-    console.log(imc)
-    //swa(`${producto.nombre} eliminado del carrito`, "DodgerBlue", 1000, 'success')
+    let imc = (masa / (altura * altura)).toFixed(2);
+    let caso = '';
+
+    if (imc < 15) {
+        caso = 'Delgadez muy Severa'
+    }
+    if (15 <= imc < 15.9) {
+        caso = 'Delgadez Severa'
+    }
+    if (15.9 <= imc < 18.4) {
+        caso = 'Delgadez'
+    }
+    if (18.4 <= imc < 24.9) {
+        caso = 'Peso Saludable'
+    }
+    else caso = 'Sobrepeso'
+
+    swa(`${nombre}\n Tu IMC es ${imc} \n Estás en ${caso}`, "DodgerBlue", 3000, 'success')
 })
